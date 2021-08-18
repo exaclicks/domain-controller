bda0b7d70d2ddda93a3cb1ac802bdb656ffd7c96
mrbulut <miksmtr@gmail.com>
2021-08-18 15:08:48 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,17 @@ use phpseclib3\System\SSH\Agent;
 // Homepage Route
 Route::get('/testercode', function () {
     $redirectServerIp = Config::get('values.REDİRECT_SERVER_IP');
-    $connection = ssh2_connect($redirectServerIp, 22);
+    $connection = ssh2_connect($redirectServerIp, 22, array('hostkey' => 'ssh-rsa'));
      $public_key_root = "/var/www/id_rsa.pub";
     $private_key_root = "/var/www/id_rsa";
-    if (ssh2_auth_agent($connection, 'root')) {
+    if (ssh2_auth_pubkey_file(
+        $connection,
+        'root@',
+        $public_key_root,
+        $private_key_root,
+        'secret'
+
+    )) {
         echo "Public Key Authentication Successful\n";
     } else {
         die('Public Key Authentication Failed');
