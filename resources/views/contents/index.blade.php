3710e73e2550c9240cf3d7a6ea3d099ebe362bca
mrbulut <miksmtr@gmail.com>
2021-08-30 14:38:16 +0200

    update

--- resources/views/contents/index.blade.php
+++ resources/views/contents/index.blade.php
@@ -40,7 +40,7 @@
     <script src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap5.min.js" ></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js" ></script>  
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
-    <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js "  defer></script>
+    <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js "  ></script>
 
   
 
